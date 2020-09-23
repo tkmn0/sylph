e3d0e72ac06b19b27b62d11600566d320440010d
nzer0 <tkmgin0914@gmail.com>
2020-09-23 00:36:29 +0000

    feat: adjust chat example

--- examples/chat/server/main.go
+++ examples/chat/server/main.go
@@ -12,14 +12,14 @@ func main() {
 	hub := util.NewHub()
 	s := sylph.NewServer("127.0.0.1", 4444)
 
-	s.OnTransport = func(t sylph.Transport) {
+	s.OnTransport(func(t sylph.Transport) {
 		t.OnChannel(func(c channel.Channel) {
 			hub.Register(c)
 		})
 		t.OnClose(func() {
 			fmt.Println("transport closed")
 		})
-	}
+	})
 
 	go s.Run()
 	hub.Chat()
