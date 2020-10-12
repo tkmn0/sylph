334e29b2a3322b0cb3897ede971c12702f87013a
nzer0 <tkmgin0914@gmail.com>
2020-10-12 09:43:13 +0900

    docs: add comment for transport

--- transport.go
+++ transport.go
@@ -4,6 +4,9 @@ import (
 	"github.com/tkmn0/sylph/pkg/channel"
 )
 
+// Transport is interface for transport.
+// Transport handles Channels.
+// A Transport handles a bundle of Channels.
 type Transport interface {
 	OpenChannel(config channel.ChannelConfig) error
 	OnChannel(handler func(channel channel.Channel))
